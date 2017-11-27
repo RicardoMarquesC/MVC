dc0ba8791de8910d7c50526f1da0a2d862c0c084
xavier <xavier.coelho.t0105279@edu.atec.pt>
2017-11-27 10:54:29 +0000

    1º commit

---
+++ EBillingDigitalMVC/ViewModels/OCRRequest/APICV.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ProjectOxford.Vision.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EBillingDigitalMVC.ViewModels.OCRRequest
+{
+    public class APICV
+    {
+
+    }    
+}
